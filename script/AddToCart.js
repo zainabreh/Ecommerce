d6952c3ec8f2b9782387be5d4b08a99242ac5d3f
Zainab <156816915+zainabreh@users.noreply.github.com>
2024-06-07 23:40:49 +0500

    working on product detail

--- script/AddToCart.js
+++ script/AddToCart.js
@@ -1,15 +1,15 @@
-window.addEventListener("load",()=>{
-    getData();
-    display();
-})
 let extractingData;
+    window.addEventListener("load",()=>{
+        getData();
+        display();
+    })
 function getData(){
     extractingData = cartItem.map((item)=>{
         for(let i=0; i<globalData.length; i++){
             if(item == globalData[i].id){
                 return globalData[i];
-            }
-        }
+                }
+                }
     })
 }
 console.log(`Get Data function is called ${getData}`);
@@ -21,55 +21,55 @@ console.log(`Extracting Data is called ${extractingData}`);
 function display(){
     let productcart = document.querySelector(".productcart");
     let providingHTML;
-
+    
     if(!extractingData || extractingData.length === 0){
         providingHTML = `<h1>No Items Added</h1>`
-    }
-     else{
-         extractingData.map((ele) => {
-               providingHTML += generatehtml(ele);
-            });
-        } 
-        productcart.innerHTML = providingHTML;
-        console.log(`Display function is called ${providingHTML}`);
-    
-}
+        }
+        else{
+            extractingData.map((ele) => {
+                providingHTML += generatehtml(ele);
+                });
+                } 
+                productcart.innerHTML = providingHTML;
+                console.log(`Display function is called ${providingHTML}`);
+                
+                }
+                
+                
+                
+                function generatehtml(prod){
+                    return `
+                    
+                    <div class="itemCard">
 
-
-
-function generatehtml(prod){
-    return `
-    
-            <div class="itemCard">
-
-                <div class="delete">
+                    <div class="delete">
                     <i class="fa-solid fa-xmark" onclick="deleteItem()"></i>
-                </div>
+                    </div>
 
                 <div class="image">
-                    <img src="${prod.image}" alt="product-image" class="prod-cart-image">
+                <img src="${prod.image}" alt="product-image" class="prod-cart-image">
                 </div>
-    
+                
                 <div class="itemDetail">
-                    
+                
                     <div class="cart-item-name">
                     ${prod.title}
                     </div>
                     
                     <div class="price">
-                        $ ${prod.price}
+                    $ ${prod.price}
                     </div>
-    
+                    
                     <div class="cart-return">
                         <span class="return-date">2 days</span> return available
                     </div>
-    
+                    
                     <div class="cart-delivery">
-                        Delivered by <span class="delivery-date">4,Nov,2024</span> 
+                    Delivered by <span class="delivery-date">4,Nov,2024</span> 
                     </div>
-    
-                </div>
+                    
+                    </div>
         </div>
-    ` 
+        ` 
 }
 
--- script/script.js
+++ script/script.js
@@ -1,73 +1,73 @@
 let container = document.querySelector(".product_container");
 let navitems = document.querySelectorAll(".nav-item")
 let count = document.querySelector(".count")
-window.addEventListener("load",()=>{
-  getProducts();
-  let bag = localStorage.getItem("cartItem")
-  cartItem = bag ? JSON.parse(bag) : [];
-  count.innerText = cartItem.length;
-})
 let cartItem = [];
-let globalData;
-function getProducts() {
-  fetch("https://fakestoreapi.com/products")
-  .then((res) => res.json())
-  .then((data) => {
-    globalData = data;
-    // console.log(data);
-    generateHTML(data);
-    filterData(data);
-  });
-}
-
-function addTocart(id){
-  cartItem.push(id);
-  localStorage.setItem('cartItem',JSON.stringify(cartItem));
-  count.innerText = cartItem.length;
-}
-console.log(`Items added to cart are ${cartItem}`);
-console.log(cartItem);
-
-function generateHTML(data) {
-  let products = "";
-  data.forEach((data) => {
-    products += `<div class="card item-container">
-          <a href="/pages/productDetail.html">
-            <img src=${data.image} class="prod-img"  alt="...">
-          </a>
-          <hr/>
-          <div class="ratings">
-            &nbsp;&nbsp;&nbsp;${data.rating.rate} ⭐ | ${data.rating.count}
-          </div>
-        <div class="card-body">
-          <h5 class="card-title">${data.title}</h5>
-          
-          <p class="card-price"><span class="price">Price:</span>&nbsp;$&nbsp;${data.price}</p>
-
-          <button class="btn btn-primary cartbtn" onclick="addTocart(${data.id})"><i class="fa-solid fa-cart-shopping">&nbsp;</i>Add to Cart</button>
-        </div>
-      </div>
-        `;
-  });
-  if(container){
-    container.innerHTML = products;
-  }
-}
-
-function filterData(data) {
-  navitems.forEach((navitem)=>{
-    navitem.addEventListener("click",(e)=>{
-      let filterData;
-      let selectedItem = e.target.textContent.trim().toLowerCase();
-
-      if(selectedItem == "all"){
-        filterData = data;
-      }else{
-        filterData = data.filter(e=>e.category.trim().toLowerCase().includes(selectedItem))
-      }
-      generateHTML(filterData)    
+  window.addEventListener("load",()=>{
+    getProducts();
+    let bag = localStorage.getItem("cartItem")
+    cartItem = bag ? JSON.parse(bag) : [];
+    count.innerText = cartItem.length;
     })
-  })
-}
-
-
+let globalData;
+async function getProducts() {
+  const res = await fetch("https://fakestoreapi.com/products")
+  const data = await res.json();
+  globalData = data;
+  // console.log(data);
+  generateHTML(data);
+  filterData(data);
+  }
+  
+  function addTocart(id){
+    cartItem.push(id);
+    localStorage.setItem('cartItem',JSON.stringify(cartItem));
+    count.innerText = cartItem.length;
+    }
+    console.log(`Items added to cart are ${cartItem}`);
+    console.log(cartItem);
+    
+    function generateHTML(data) {
+      let products = "";
+      data.forEach((data) => {
+        products += `<div class="card item-container">
+        <a href="/pages/productDetail.html">
+        <img src=${data.image} class="prod-img"  alt="...">
+        </a>
+        <hr/>
+        <div class="ratings">
+        &nbsp;&nbsp;&nbsp;${data.rating.rate} ⭐ | ${data.rating.count}
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">${data.title}</h5>
+        
+        <p class="card-price"><span class="price">Price:</span>&nbsp;$&nbsp;${data.price}</p>
+        
+        <button class="btn btn-primary cartbtn" onclick="addTocart(${data.id})"><i class="fa-solid fa-cart-shopping">&nbsp;</i>Add to Cart</button>
+        </div>
+        </div>
+        `;
+        });
+        if(container){
+          container.innerHTML = products;
+          }
+          }
+          
+          function filterData(data) {
+            navitems.forEach((navitem)=>{
+              navitem.addEventListener("click",(e)=>{
+                let filterData;
+                let selectedItem = e.target.textContent.trim().toLowerCase();
+                
+                if(selectedItem == "all"){
+                  filterData = data;
+                  }else{
+              filterData = data.filter(e=>e.category.trim().toLowerCase().includes(selectedItem))
+              }
+              generateHTML(filterData)    
+              })
+              })
+              }
+              
+              
+              
+              // localStorage.clear()
